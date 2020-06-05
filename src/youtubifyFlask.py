28e82b3b2609fa78b4108aa5200f118e8cc0b277
Gage Greenhouse <gsggage@yahoo.com>
2020-06-04 23:29:20 -0500

    made a decent amount of progress

--- src/youtubifyFlask.py
+++ src/youtubifyFlask.py
@@ -4,12 +4,15 @@
 # Now lets get banned
 
 from flask import Flask, request
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 import requests
 from bs4 import BeautifulSoup
 import subprocess
-from typing import NamedTuple
+import youtube_dl
 
+
+queue = []
+lastSearchPage = ""
 
 def openHTML():
 	basicPage = ''' <!DOCTYPE html>
@@ -25,7 +28,9 @@ def headHTML():
 								float:left;
 								width:160px;
 								height:90px;
-								margin-bottom:10px;
+								margin-bottom:5px;
+								margin-top:5px;
+								margin-right:10px;
 							}
 							.rightboxtop {
 								width:100%;
@@ -45,6 +50,9 @@ def headHTML():
 								width:100%;
 								display:inline-block;
 							}
+							#player {
+								width:100%
+							}
 						</style>
 					</head>
 				'''
@@ -59,7 +67,7 @@ def homeHTML():
 						<h1>Youtube/Spotify</h1>
 						<iframe src="searchPage" height="400" width="100%"></iframe>
 						<h2>Currently Playing: </h2>
-						<iframe src="queue" height="400" width="20%"></iframe>
+						<iframe src="nowPlaying" height="400" width="100%"></iframe>
 					</body>
 				'''
 	return openHTML()+headHTML()+basicPage+closeHTML()
@@ -75,6 +83,10 @@ def searchFrame():
 	return openHTML()+headHTML()+basicPage+closeHTML()
 def searchHTML(searchTerm, ids, names, URLs):
 	basicPage = '''	<body>
+						<form method="POST" action="search">
+							<input name="text" value="'''+searchTerm+'''">
+							<input type="submit">
+						</form>
 						<p>Search Results for "'''+searchTerm+'''"</p>
 						<p><a href="searchPage">Click here to return to search page</a></p>
 				'''
@@ -83,7 +95,7 @@ def searchHTML(searchTerm, ids, names, URLs):
 	basicPage += ''' </body>
 				'''
 	return openHTML()+headHTML()+basicPage+closeHTML()
-def generateQueueItem(vidID,name,url):
+def generateQueueItem(vidID,name,url,queueNum,isForQueue):
 	basicPage = '''	<div class="main">
 						<div class="leftbox">
 							<img src='https://img.youtube.com/vi/'''+vidID+'''/maxresdefault.jpg' width="160" height="90">
@@ -93,15 +105,59 @@ def generateQueueItem(vidID,name,url):
 								<h3><a href="'''+url+'''">'''+name+'''</a></h3>
 							</div>
 							<div class="rightboxbot">
-								<form method="POST" action="addSong">
-									<input type="submit" value="Add Song">
-									<input type="submit" value="Add Front of Queue">
+								<form method="POST" action="'''
+								
+								
+	if isForQueue == True:
+		basicPage += "queuePage"
+	else:
+		basicPage += "nowPlaying"
+	
+	basicPage += '''">
+									<input type="hidden" name="vidID" value="'''+vidID+'''">
+									<input type="hidden" name="songName" value="'''+name+'''">
+									<input type="hidden" name="url" value="'''+url+'''">
+									<input type="hidden" name="queueNum" value="'''+str(queueNum)+'''">
+									<input type="submit" name="submit" value="Remove Song">
 								</form>
 							</div>
 						</div>
 					</div>
 				'''
 	return basicPage
+
+def generateNowPlaying():
+	
+	if len(queue) != 0:
+		basicPage = '''	<audio controls autoplay id="player">
+							<source src="/static/'''+queue[0][0]+'''.mp3" type="audio/mpeg">
+						</audio>
+					'''
+		basicPage += generateQueueItem(queue[0][0],queue[0][1],queue[0][2],0,False)
+		basicPage += '''<form method='POST' action="nowPlaying">
+							<input type="submit" name="submit" value="Next" width="100%">
+						</form>
+						'''
+		count = 1
+		for i in queue[1:]:
+			basicPage += generateQueueItem(i[0],i[1],i[2],count,True)
+			count+=1
+		
+	else:
+		basicPage='''	<head>
+							<meta http-equiv="refresh" content="1">
+						</head>'''
+		
+	return openHTML()+headHTML()+basicPage+closeHTML()
+	
+def generateQueue():
+	basicPage=""
+	count = 1
+	for i in queue[1:]:
+		basicPage += generateQueueItem(i[0],i[1],i[2],count,True)
+		count+=1
+		
+	return openHTML()+headHTML()+basicPage+closeHTML()
 def generateResult(vidID,name,url):
 	basicPage = '''	<div class="main">
 						<div class="leftbox">
@@ -113,8 +169,11 @@ def generateResult(vidID,name,url):
 							</div>
 							<div class="rightboxbot">
 								<form method="POST" action="addSong">
-									<input type="submit" value="Add Song">
-									<input type="submit" value="Add Front of Queue">
+									<input type="hidden" name="vidID" value="'''+vidID+'''">
+									<input type="hidden" name="songName" value="'''+name+'''">
+									<input type="hidden" name="url" value="'''+url+'''">
+									<input type="submit" name="submit" value="Add Song">
+									<input type="submit" name="submit" value="Add Front of Queue">
 								</form>
 							</div>
 						</div>
@@ -126,10 +185,15 @@ def generateResult(vidID,name,url):
 	
 def fsearchHTML():
 	basicPage = '''	<body>
+						<form method="POST" action="search">
+							<input name="text">
+							<input type="submit">
+						</form>
 						<p>Search Failed</p>
+						<p><a href="searchPage">Click here to return to search page</a></p>
 					</body>
 				'''
-	return openHTML()+headHTML()+basicPage+closseHTML()
+	return openHTML()+headHTML()+basicPage+closeHTML()
 @app.route('/')
 def root():
 	return homeHTML()
@@ -138,18 +202,39 @@ def searchPage():
 	return searchFrame()
 @app.route('/addSong', methods=['POST'])
 def addSong():
+	global lastSearchPage
 	
+	queueItem = [request.form['vidID'],request.form['songName'],request.form['url']]
 	
+	# Add Code to download the audio of the song in the queue.
+	subprocess.run(["youtube-dl",request.form['url'],"-f 140","-x","--audio-format","mp3","-o","static/"+request.form['vidID']+".mp3"])
 	
+	if request.form['submit'] == "Add Song":
+		print("Add Song")
+		queue.append(queueItem)
+	elif request.form['submit'] == "Add Front of Queue":
+		print("Add Song to Front of Queue")
+		queue.insert(0,queueItem)
+	return lastSearchPage
 	
-	return searchFrame()
+@app.route('/nowPlaying', methods=['GET','POST'])
+def nowPlaying():
+	if request.method == 'POST':
+		if request.form['submit'] == "Remove Song":
+			queue.remove(queue[int(request.form['queueNum'])])
+		if request.form['submit'] == "Next":
+			temp = queue[0]
+			queue.remove(temp)
+			queue.append(temp)
+	return generateNowPlaying()
+
+@app.route('/queuePage', methods=['GET','POST'])
+def queuePage():
 	
-@app.route('/queue')
-def queue():
-	
-	return searchFrame()
+	return generateQueue()
 @app.route('/search', methods=['POST'])
 def search():
+	global lastSearchPage
 	st = request.form['text']
 	ytURL = generateSearchURL(st)
 	page = requests.get(ytURL)
@@ -161,9 +246,14 @@ def search():
 	names = []
 	URLs = []
 	results = list(soup.findAll('a',attrs={'class':'yt-uix-tile-link'}))
-	if len(results)<5:
+	if len(results)<1:
 		return fsearchHTML()
-	for i in range(5):
+		
+	numResults = len(results)
+	if numResults > 20:
+		numresults = 20
+	
+	for i in range(numResults):
 		foundString = str(results[i])
 		offset = foundString.find("href")
 		ids.append(foundString[offset+15:offset+26])
@@ -172,14 +262,9 @@ def search():
 		names.append(foundString[offset1+7:offset2])
 		URLs.append('https://youtube.com/watch?v='+ids[i])
 		
+	lastSearchPage = searchHTML(st,ids,names,URLs)
+	return lastSearchPage
 	
-	return searchHTML(st,ids,names,URLs)
-	
-	
-	
-	
-	
-	return searchHTML()
 def generateSearchURL(searchTerm):
 	return '''https://youtube.com/results?search_query='''+searchTerm.replace(' ','+')
 if __name__ == '__main__':
